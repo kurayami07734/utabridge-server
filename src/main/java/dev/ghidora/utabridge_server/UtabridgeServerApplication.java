0b81e5ec7267fc5b1832d478914e6182ffd566d8
Aditya Ghidora <adityaghidora@gmail.com>
2025-10-28 11:50:15 +0530

    feat: Add spring boot starter code

---
+++ src/main/java/dev/ghidora/utabridge_server/UtabridgeServerApplication.java
@@ -0,0 +1,13 @@
+package dev.ghidora.utabridge_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtabridgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtabridgeServerApplication.class, args);
+	}
+
+}
