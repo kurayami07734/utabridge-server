17f10b52b684eda0529df922c60546027215fea8
Aditya Ghidora <adityaghidora@gmail.com>
2026-01-11 19:57:28 +0530

    feat: Persist user on making request to auth route

--- src/main/java/dev/ghidora/utabridge_server/controllers/AuthController.java
+++ src/main/java/dev/ghidora/utabridge_server/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package dev.ghidora.utabridge_server.controllers;
 
 import dev.ghidora.utabridge_server.enums.IdentityProvider;
+import dev.ghidora.utabridge_server.repositories.UserRepository;
+import dev.ghidora.utabridge_server.services.AuthService;
 import dev.ghidora.utabridge_server.services.IdentityTokenVerifier;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,26 +16,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final IdentityTokenVerifier identityTokenVerifier;
+    private final AuthService authService;
 
     @Autowired
-    public AuthController(IdentityTokenVerifier identityTokenVerifier) {
+    public AuthController(IdentityTokenVerifier identityTokenVerifier, AuthService authService) {
         this.identityTokenVerifier = identityTokenVerifier;
+        this.authService = authService;
     }
 
     public record AuthRequest(
             @NotBlank String token,
             @NotBlank IdentityProvider provider
-    ) {}
+    ) {
+    }
 
-    public record AuthResponse(String token) {}
+    public record AuthResponse(String token) {
+    }
 
     @PostMapping()
     public ResponseEntity<AuthResponse> handleAuth(@RequestBody AuthRequest payload) {
         // TODO: respect provider when adding support for discord login
         try {
             var verifiedUser = identityTokenVerifier.verifyToken(payload.token());
+
+            var user = authService.getOrCreateUser(
+                    verifiedUser.email(),
+                    verifiedUser.name(),
+                    verifiedUser.pictureUrl(),
+                    verifiedUser.providerId()
+            );
+
             return ResponseEntity.ok().body(new AuthResponse(payload.token()));
-        } catch(Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
---
+++ src/main/java/dev/ghidora/utabridge_server/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.ghidora.utabridge_server.repositories;
+
+import dev.ghidora.utabridge_server.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
