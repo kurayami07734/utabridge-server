509fe24f0af5208bfa0eff88f15f7dab787a23c3
Aditya Ghidora <adityaghidora@gmail.com>
2026-01-21 17:40:50 +0530

    feat: Add last active timestamp to user

--- src/main/java/dev/ghidora/utabridgeserver/repositories/UserRepository.java
+++ src/main/java/dev/ghidora/utabridgeserver/repositories/UserRepository.java
@@ -1,12 +1,20 @@
 package dev.ghidora.utabridgeserver.repositories;
 
 import dev.ghidora.utabridgeserver.models.User;
+import java.time.Instant;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /** Repository for User entity. */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
+
+  @Modifying
+  @Query("UPDATE User u SET u.lastActiveAt = :time WHERE u.id = :id")
+  void updateLastActiveAt(@Param("id") Long id, @Param("time") Instant time);
 }
