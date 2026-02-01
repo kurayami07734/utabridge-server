d4d2ae95b34d29fcf4c134d49a6978173ad4f903
Aditya Ghidora <adityaghidora@gmail.com>
2026-02-01 22:27:26 +0530

    fix: Allow swagger in dev only

--- src/main/java/dev/ghidora/utabridgeserver/UtabridgeServerApplication.java
+++ src/main/java/dev/ghidora/utabridgeserver/UtabridgeServerApplication.java
@@ -2,10 +2,11 @@ package dev.ghidora.utabridgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /** Main application class for Utabridge Server. */
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableScheduling
 public class UtabridgeServerApplication {
   public static void main(String[] args) {
