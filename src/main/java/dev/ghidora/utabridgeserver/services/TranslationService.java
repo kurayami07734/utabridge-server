efeb7759f98b3543873a79bb60d802d46d687322
Aditya Ghidora <adityaghidora@gmail.com>
2026-02-09 23:10:28 +0530

    refactor: Move language detection to server

--- src/main/java/dev/ghidora/utabridgeserver/services/TranslationService.java
+++ src/main/java/dev/ghidora/utabridgeserver/services/TranslationService.java
@@ -7,6 +7,13 @@ import dev.ghidora.utabridgeserver.exceptions.TranslationException;
  * from a specific provider (e.g., Google, Microsoft, etc.).
  */
 public interface TranslationService {
+  /**
+   * Translation Result record.
+   *
+   * @param translatedText The translated text.
+   * @param detectedLanguage The detected source language.
+   */
+  record TranslatedText(String translatedText, String detectedLanguage) {}
 
   /**
    * Translates text from a source language to a target language.
@@ -17,8 +24,27 @@ public interface TranslationService {
    * @return The translated text.
    * @throws TranslationException if translation fails or language is not supported.
    */
-  String translateText(String text, String sourceLanguage, String targetLanguage)
+  TranslatedText translateText(String text, String sourceLanguage, String targetLanguage)
       throws TranslationException;
+
+  /**
+   * Translates text from a source language to a target language. Source Language will be
+   * auto-detected
+   *
+   * @param text The text to translate.
+   * @param targetLanguage The ISO 639-1 code for the target language (e.g., "hi").
+   * @return The translated text.
+   * @throws TranslationException if translation fails or language is not supported.
+   */
+  TranslatedText translateText(String text, String targetLanguage) throws TranslationException;
+
+  /**
+   * Romanization Result record.
+   *
+   * @param romanizedText The romanized text.
+   * @param detectedLanguage The detected source language.
+   */
+  record RomanizedText(String romanizedText, String detectedLanguage) {}
 
   /**
    * Romanizes text from a language that uses a non-Latin script.
@@ -28,5 +54,15 @@ public interface TranslationService {
    * @return The romanized text (e.g., "namaste").
    * @throws TranslationException if romanization fails or language is not supported.
    */
-  String romanizeText(String text, String sourceLanguage) throws TranslationException;
+  RomanizedText romanizeText(String text, String sourceLanguage) throws TranslationException;
+
+  /**
+   * Romanizes text from a language that uses a non-Latin script. Source language will be
+   * auto-detected
+   *
+   * @param text The text to romanize (e.g., "नमस्ते").
+   * @return The romanized text (e.g., "namaste").
+   * @throws TranslationException if romanization fails or language is not supported.
+   */
+  RomanizedText romanizeText(String text) throws TranslationException;
 }
